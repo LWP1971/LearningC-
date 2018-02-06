91dece1e7c7a660edaa39f4d67d6ea95554ae586
Lasse1971 <lwp@wihlborgrail.dk>
2018-02-06 21:42:45 +0100

    aaa

---
+++ Projects/TestOfArrays/TestOfArrays/Arrays.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TestOfArrays
+{
+    public class Arrays
+    {
+        public int[] firstArray =
+        {1,2,3,4,5};
+
+        public string[] secondArray =
+        {"Arne","Bjarne","Christian","Dennis","Erik"};
+
+        public Random i = new Random();
+
+        public int SelectName()
+        {
+            int a = i.Next (1 ,firstArray.Length);
+            return a;
+        }
+    }
+}
---
+++ Projects/TestOfArrays/TestOfArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestOfArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrays = new Arrays();
+            var gotTheRandom = arrays.SelectName();
+
+            Console.WriteLine($"Nummer {arrays.firstArray[gotTheRandom]} på listen over navne hedder {arrays.secondArray[gotTheRandom]}");
+            Console.ReadKey();
+
+            //);
+            //Console.ReadKey();
+        }
+    }
+}
+
