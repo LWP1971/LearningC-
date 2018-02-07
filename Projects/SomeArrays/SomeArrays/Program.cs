3fa90b32d0b11725c6ed3d5c697b6421305770c5
Lasse Parkov <lwp@wihlborgrail.dk>
2018-02-07 10:34:47 +0100

    ccc

---
+++ Projects/SomeArrays/SomeArrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SomeArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TheArrays test = new TheArrays();
+
+            Console.WriteLine(test.anArray [1,1]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projects/SomeArrays/SomeArrays/TheArrays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SomeArrays
+{
+    class TheArrays
+    {
+        public int Value { get; set; }
+
+        public int[,] anArray = new int[,]
+        {
+            {1,2,3,4,5,},
+            {10,20,30,40,50}
+        };
+        
+    }
+}
