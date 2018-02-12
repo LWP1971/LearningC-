b2ab2633afe7134e40a41b7ab567308594009e99
Lasse Parkov <lwp@wihlborgrail.dk>
2018-02-12 20:18:05 +0100

    aaa

--- Projects/TestOfArrays/TestOfArrays/Program.cs
+++ Projects/TestOfArrays/TestOfArrays/Program.cs
@@ -7,16 +7,12 @@ namespace TestOfArrays
     {
         static void Main(string[] args)
         {
-            var playingCard = new PlayingCard();
-            var deckOfCards = new DeckOfCards();
-
+            
             bool continueProgram = false;
 
             while (!continueProgram)
             {
-                Console.WriteLine(playingCard.MakeACard());
-
-                deckOfCards = new DeckOfCards();
+                var deckOfCards = new DeckOfCards();
                 deckOfCards.Populate();
 
                 Console.ReadKey();
